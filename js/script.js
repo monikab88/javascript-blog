5683c33ff84d53a80a6ba16bedcc90ae22ea48c7
Monika Bielicz <bieliczm@gmail.com>
2022-07-05 16:17:10 +0200

    change script

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
     console.log('links:', links);
   }); 
 */
+const titleClickHandler = function(){
+  console.log('Link was clicked!');
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
