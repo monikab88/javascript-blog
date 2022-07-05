61e7c265bb066a0d4e9d2c4edd4c5634ca1a4d26
Monika Bielicz <bieliczm@gmail.com>
2022-07-05 16:34:18 +0200

    change script

--- js/script.js
+++ js/script.js
@@ -3,8 +3,21 @@
     console.log('links:', links);
   }); 
 */
-const titleClickHandler = function(){
-  console.log('Link was clicked!');
+
+const titleClickHandler = function(event){
+  console.log(event 'Link was clicked!');
+
+  /* remove class 'active' from all article links  */
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
