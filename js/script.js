e4736a557e4de79aa421bb73885f395ff335e462
Monika Bielicz <bieliczm@gmail.com>
2022-07-05 17:44:05 +0200

    change script

--- js/script.js
+++ js/script.js
@@ -5,17 +5,21 @@
 */
 
 const titleClickHandler = function(event){
-  console.log('Link was clicked!');
+  const clickedElement = this;
+  console.log('Link was clicked!', event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+
+  console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('.articles a.active');
 
   for(let activeArticle of activeArticles){
