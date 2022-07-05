0568a45967fe3e4c818bf1aa4cb3569db0ffec76
Monika Bielicz <bieliczm@gmail.com>
2022-07-05 16:49:14 +0200

    option remove

--- js/script.js
+++ js/script.js
@@ -8,7 +8,11 @@ const titleClickHandler = function(event){
   console.log('Link was clicked!', event);
 
   /* remove class 'active' from all article links  */
+const activeLinks = document.querySelectorAll('.titles a.active');
 
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
