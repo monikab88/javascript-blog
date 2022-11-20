96f8c605e73d341f92a22522f095933fd84ca072
Monika Bielicz <bieliczm@gmail.com>
2022-11-20 02:12:35 +0100

    js taglink

--- js/script.js
+++ js/script.js
@@ -138,7 +138,7 @@ function generateTags() {
   /* [NEW] START LOOP: for each tag in allTags: */
   for(let tag in allTags){
   /* [NEW] generate code of a link and add it to allTagsHTML */
-  allTagsHTML += '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';  
+  allTagsHTML += '<li><a href="#tag-' + tag + '(' + allTags[tag] + ')">' + tag + '</a></li>';  
   /* [NEW] END LOOP: for each tag in allTags: */
 }
   /*[NEW] add HTML from allTagsHTML to tagList */
